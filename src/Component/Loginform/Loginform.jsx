22ec3de3b702c3668a4d246eb768dd3f042c5238
Ines-05 <inestchekpo5@gmail.com>
2024-03-12 01:01:43 +0100

    new agttibutes

--- src/Component/Loginform/Loginform.jsx
+++ src/Component/Loginform/Loginform.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import './Loginform.css';
 const Loginform = () =>{
     return (
         <div>
-            LoginForm
+            <h1>Bienvenue</h1>
+        <div className='wrapper'>
+           
+                
+                <div className='input-box'>
+                    <button className='Button'><a href="#">Voir les résultats</a></button>
+                </div>
+                <div className='input-box'>
+                    <button className='Button'><a href="#"> Paiement</a></button>
+                </div>
+               
+        </div>
+        <button className='decon'>Déconnexion</button>
         </div>
     )
 };
