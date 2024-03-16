92e536521d5ac3809dd65f8e1d3df75052c7f648
Ines-05 <inestchekpo5@gmail.com>
2024-03-16 20:19:16 +0100

    Results files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Loginform from './Component/Loginform/Loginform';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Loginform/>
-    </div>
+    
+      <div>
+        <Loginform/>
+      </div>
+      
+  
+    
   );
 }
 
---
+++ src/Component/resultats/resultats.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {link} from 'react-router-dom'
+
+const Result = () =>{
+    return (
+        <div>
+            <h1>Voir les résultats</h1>
+            <div className='rewrap'>
+                <div>
+                    <form action="">
+                        Identifiant : <br />
+                        <input type="text" />
+                    </form>
+                </div>
+                <div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
